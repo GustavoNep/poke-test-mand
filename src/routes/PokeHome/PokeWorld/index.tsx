83f6c04719ddc3ee83072d9e216fb63db94b2c60
Gustavo Nep <gustavo._12@hotmail.com>
2023-08-30 19:17:13 -0300

    title pokeworld page

--- src/routes/PokeHome/PokeWorld/index.tsx
+++ src/routes/PokeHome/PokeWorld/index.tsx
@@ -1,11 +1,16 @@
-
+import './styles.css'
 
 export default function PokeWorld() {
 
     return(
         <main>
-            <section>
-                <h2>Poke Section</h2>
+            <section className='pokemon'>
+                <div className='pokemon__title'>
+                    <h1>Mandarin Poke World</h1>
+                </div>
+                <div className='pokemon__box'>
+                    <h1>Pokemon Box</h1>
+                </div>
             </section>
         </main>
     )
